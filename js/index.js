b71ac1979a9cdfc80cfbb35f89f1d3e70ef98977
Rosa <costurezati@gmail.com>
2026-02-13 08:02:25 -0300

    .

---
+++ js/index.js
@@ -0,0 +1,66 @@
+//Ativar cliques no menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+  const url = location.href;
+  const href = link.href;
+  
+  if(url.includes (href)){
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+//Ativar cliques no orçamento
+
+const parametros = new URLSearchParams (location.search);
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro);
+  if(elemento){
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');  
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".cam-img img");
+const galeriaContainer = document.querySelector('.cam-img');
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 100px)').matches;
+
+  if(media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img){
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
+
+//Animação
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
